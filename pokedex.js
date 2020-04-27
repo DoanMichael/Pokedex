42d1665cbc162d6b146c10d68ef6019412ed1cb0
Michael Doan <37003756+DoanMichael@users.noreply.github.com>
2020-04-27 01:05:32 -0600

    Add files via upload
    Finished Pokedex

--- pokedex.js
+++ pokedex.js
@@ -1,6 +1,8 @@
 const pokedex = document.getElementById("pokedex");
 console.log(pokedex);
 
+//retrieves all Pokemon up to #493 (in order)
+//prototype pattern
 const getPokemon = () => 
 {
     const entries = [];
@@ -25,6 +27,69 @@ const getPokemon = () =>
     });
 };
 
+//sorts Pokemon based on the their type in alphabetical order (of type)
+const typePokemon = () => {
+    const promises = [];
+    for (let i = 1; i <= 493; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((result) => (
+        {
+            name: result.name,
+            id: result.id,
+            image: result.sprites['front_default'],
+            type: result.types.map((type) => type.type.name).join(', '),
+            height: result.height/10,
+            weight: result.weight/10
+        })).sort((a, b) => a.type > b.type ? 1 : -1);
+        displayPokemon(pokemon);
+    });
+};
+
+//sorts height of Pokemon based on their height 
+const heightPokemon = () => {
+    const promises = [];
+    for (let i = 1; i <= 493; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((result) => (
+        {
+            name: result.name,
+            id: result.id,
+            image: result.sprites['front_default'],
+            type: result.types.map((type) => type.type.name).join(', '),
+            height: result.height/10,
+            weight: result.weight/10
+        })).sort((a, b) => a.height > b.height ? 1 : -1);
+        displayPokemon(pokemon);
+    });
+};
+
+//sorts Pokemon based on their weight
+const weightPokemon = () => {
+    const promises = [];
+    for (let i = 1; i <= 493; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((result) => (
+        {
+            name: result.name,
+            id: result.id,
+            image: result.sprites['front_default'],
+            type: result.types.map((type) => type.type.name).join(', '),
+            height: result.height/10,
+            weight: result.weight/10
+        })).sort((a, b) => a.weight > b.weight ? 1 : -1);
+        displayPokemon(pokemon);
+    });
+};
+
 const displayPokemon = (pokemon) =>
 {
     console.log(pokemon);
@@ -40,4 +105,16 @@ const displayPokemon = (pokemon) =>
     pokedex.innerHTML = pokemonHTML;
 }; 
 
-getPokemon();
+//strategy pattern (sorts Pokemon displayed depending on picked)
+window.test = function(e) {
+//observer pattern(s) (whenever one of the sorts is picked, it notifies the program and calls another method)
+  if (e.value === 'Number') {
+    getPokemon();
+  } else if (e.value === 'Type') {
+    typePokemon();
+  } else if (e.value === 'Height') {
+    heightPokemon();
+  } else if (e.value === 'Weight') {
+    weightPokemon();
+  }
+};
